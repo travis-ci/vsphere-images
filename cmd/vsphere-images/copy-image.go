e1d97b9aa6e3e75bd49ec2a27a769d65d9bd248b
Henrik Hodne <henrik@travis-ci.com>
2017-01-19 22:48:16 +0000

    Move the progress logger to its own file
    It's not really specific to the copy-image command.

--- cmd/vsphere-images/copy-image.go
+++ cmd/vsphere-images/copy-image.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"io"
 	"net/url"
-	"os"
 	"path"
-	"sync"
-	"time"
 
 	"github.com/pkg/errors"
 	vsphereimages "github.com/travis-ci/vsphere-images"
 	"github.com/urfave/cli"
-	"github.com/vmware/govmomi/vim25/progress"
 )
 
 var copyImageCommand = cli.Command{
@@ -111,98 +105,4 @@ func copyImageAction(c *cli.Context) error {
 	logger.Wait()
 
 	return nil
-}
-
-type progressLogger struct {
-	prefix string
-	wg     sync.WaitGroup
-
-	sink chan chan progress.Report
-	done chan struct{}
-}
-
-func newProgressLogger(prefix string) *progressLogger {
-	p := &progressLogger{
-		prefix: prefix,
-
-		sink: make(chan chan progress.Report),
-		done: make(chan struct{}),
-	}
-
-	p.wg.Add(1)
-
-	go p.loopA()
-
-	return p
-}
-
-func (p *progressLogger) loopA() {
-	var err error
-
-	defer p.wg.Done()
-
-	tick := time.NewTicker(100 * time.Millisecond)
-	defer tick.Stop()
-
-	for stop := false; !stop; {
-		select {
-		case ch := <-p.sink:
-			err = p.loopB(tick, ch)
-			if err != nil {
-				stop = true
-			}
-		case <-p.done:
-			stop = true
-		case <-tick.C:
-			fmt.Fprintf(os.Stderr, "\r%s      ", p.prefix)
-		}
-	}
-
-	if err != nil && err != io.EOF {
-		fmt.Fprintf(os.Stderr, "\r%s      ", p.prefix)
-		fmt.Fprintf(os.Stderr, "\r%sError: %s\n", p.prefix, err)
-	} else {
-		fmt.Fprintf(os.Stderr, "\r%s      ", p.prefix)
-		fmt.Fprintf(os.Stderr, "\r%sOK\n", p.prefix)
-	}
-}
-
-func (p *progressLogger) loopB(tick *time.Ticker, ch <-chan progress.Report) error {
-	var r progress.Report
-	var ok bool
-	var err error
-
-	for ok = true; ok; {
-		select {
-		case r, ok = <-ch:
-			if !ok {
-				break
-			}
-			err = r.Error()
-		case <-tick.C:
-			line := "\r" + p.prefix
-			if r != nil {
-				line += fmt.Sprintf("(%.0f%%", r.Percentage())
-				detail := r.Detail()
-				if detail != "" {
-					line += fmt.Sprintf(", %s", detail)
-				}
-				line += ")"
-			}
-			fmt.Fprintf(os.Stderr, "%s", line)
-		}
-	}
-
-	return err
-}
-
-func (p *progressLogger) Sink() chan<- progress.Report {
-	ch := make(chan progress.Report)
-	p.sink <- ch
-	return ch
-}
-
-func (p *progressLogger) Wait() {
-	close(p.done)
-	p.wg.Wait()
 }
