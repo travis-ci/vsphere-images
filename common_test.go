5f641c6f973f40e00250da6b93cafc3ab56f23fa
Matt Moriarity <matt@mattmoriarity.com>
2018-07-26 18:09:09 -0500

    Add tests for moving images
    Added some common infrastructure for running unit tests against a
    simulated vSphere instance

---
+++ common_test.go
@@ -0,0 +1,111 @@
+package vsphereimages
+
+import (
+	"net/url"
+	"sync"
+
+	"github.com/vmware/govmomi/simulator"
+	"github.com/vmware/govmomi/vim25/progress"
+)
+
+// copied from the real progress logger for vsphere-images commands
+// which in turn is copied from the govc progress logger
+//
+// this one is modified to not print anything
+// the Sinker interface is surprisingly complicated to implement correctly
+
+type progressLogger struct {
+	wg sync.WaitGroup
+
+	sink chan chan progress.Report
+	done chan struct{}
+}
+
+func newProgressLogger() *progressLogger {
+	p := &progressLogger{
+		sink: make(chan chan progress.Report),
+		done: make(chan struct{}),
+	}
+
+	p.wg.Add(1)
+
+	go p.loopA()
+
+	return p
+}
+
+func (p *progressLogger) loopA() {
+	var err error
+
+	defer p.wg.Done()
+
+	for stop := false; !stop; {
+		select {
+		case ch := <-p.sink:
+			err = p.loopB(ch)
+			if err != nil {
+				stop = true
+			}
+		case <-p.done:
+			stop = true
+		}
+	}
+}
+
+func (p *progressLogger) loopB(ch <-chan progress.Report) error {
+	var r progress.Report
+	var ok bool
+	var err error
+
+	for ok = true; ok; {
+		select {
+		case r, ok = <-ch:
+			if !ok {
+				break
+			}
+			err = r.Error()
+		}
+	}
+
+	return err
+}
+
+func (p *progressLogger) Sink() chan<- progress.Report {
+	ch := make(chan progress.Report)
+	p.sink <- ch
+	return ch
+}
+
+func (p *progressLogger) Wait() {
+	close(p.done)
+	p.wg.Wait()
+}
+
+type SimulatedService struct {
+	Model  *simulator.Model
+	Server *simulator.Server
+}
+
+func StartService() (*SimulatedService, error) {
+	model := simulator.VPX()
+	model.Machine = 4
+	if err := model.Create(); err != nil {
+		return nil, err
+	}
+	s := model.Service.NewServer()
+
+	service := &SimulatedService{
+		Model:  model,
+		Server: s,
+	}
+	return service, nil
+}
+
+func (service *SimulatedService) Stop() {
+	service.Server.Close()
+	service.Model.Remove()
+}
+
+func (service *SimulatedService) URL() *url.URL {
+	return service.Server.URL
+}
---
+++ image_modifier_test.go
@@ -0,0 +1,122 @@
+package vsphereimages
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/find"
+)
+
+func TestMoveImageTopLevel(t *testing.T) {
+	service, err := StartService()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer service.Stop()
+
+	ctx := context.TODO()
+	logger := newProgressLogger()
+	if err = MoveImage(ctx, service.URL(), false, "/DC0/vm/DC0_H0_VM0", "/DC0/vm", "my_renamed_vm", logger); err != nil {
+		t.Fatal(err)
+	}
+	logger.Wait()
+}
+
+func TestMoveImageIntoFolder(t *testing.T) {
+	service, err := StartService()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer service.Stop()
+
+	ctx := context.TODO()
+	if err = createFolder(ctx, service, "/DC0/vm", "test_folder"); err != nil {
+		t.Fatal(err)
+	}
+
+	logger := newProgressLogger()
+	if err = MoveImage(ctx, service.URL(), false, "/DC0/vm/DC0_H0_VM0", "/DC0/vm/test_folder", "my_renamed_vm", logger); err != nil {
+		t.Fatal(err)
+	}
+	logger.Wait()
+}
+
+func TestMoveImageIntoNonexistentFolder(t *testing.T) {
+	service, err := StartService()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer service.Stop()
+
+	ctx := context.TODO()
+	logger := newProgressLogger()
+	err = MoveImage(ctx, service.URL(), false, "/DC0/vm/DC0_H0_VM0", "/DC0/vm/test_folder", "my_renamed_vm", logger)
+	if err == nil {
+		t.Fatal("expected error moving image, but none occurred")
+	}
+	if !strings.Contains(err.Error(), "finding the destination folder failed") {
+		t.Fatal(err)
+	}
+	logger.Wait()
+}
+
+func TestMoveImageThatDoesNotExist(t *testing.T) {
+	service, err := StartService()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer service.Stop()
+
+	ctx := context.TODO()
+	logger := newProgressLogger()
+	defer logger.Wait()
+	err = MoveImage(ctx, service.URL(), false, "/DC0/vm/nonexistent_vm", "/DC0/vm", "my_renamed_vm", logger)
+
+	if err == nil {
+		t.Fatal("expected error moving image, but none occurred")
+	}
+	if !strings.Contains(err.Error(), "finding the VM failed") {
+		t.Fatal(err)
+	}
+}
+
+func TestMoveImageWithoutRename(t *testing.T) {
+	service, err := StartService()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer service.Stop()
+
+	ctx := context.TODO()
+	if err = createFolder(ctx, service, "/DC0/vm", "test_folder"); err != nil {
+		t.Fatal(err)
+	}
+
+	logger := newProgressLogger()
+	if err = MoveImage(ctx, service.URL(), false, "/DC0/vm/DC0_H0_VM0", "/DC0/vm/test_folder", "DC0_H0_VM0", logger); err != nil {
+		t.Fatal(err)
+	}
+	logger.Wait()
+}
+
+func createFolder(ctx context.Context, service *SimulatedService, location string, name string) error {
+	client, err := govmomi.NewClient(ctx, service.URL(), false)
+	if err != nil {
+		return err
+	}
+
+	finder := find.NewFinder(client.Client, false)
+
+	folder, err := finder.Folder(ctx, location)
+	if err != nil {
+		return err
+	}
+
+	if _, err = folder.CreateFolder(ctx, name); err != nil {
+		return err
+	}
+
+	return nil
+}
